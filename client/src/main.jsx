b93b1b6af8580af743354a74d93563f38ef57917
Hayat ilyas <ihayat855@gmail.com>
2023-04-08 15:41:51 +0530

    add context-provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import AppContextProvider from "./Context/AppContextProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppContextProvider>
     <App />
-  </React.StrictMode>,
-)
+  </AppContextProvider>
+);
