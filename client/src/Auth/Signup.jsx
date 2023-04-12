3cff54437f40a0e7133628a69703139f42939f4b
Hayat ilyas <ihayat855@gmail.com>
2023-04-12 15:05:55 +0530

    adding name field

--- client/src/Auth/Signup.jsx
+++ client/src/Auth/Signup.jsx
@@ -1,22 +1,29 @@
 import React, { Fragment, useState } from "react";
-import { BASE_URL } from "../globalConfig";
+import { BASE_URL, toastifyoption } from "../globalConfig";
+import {toast} from 'react-toastify'
+import {useNavigate} from 'react-router-dom'
 
 function Signup() {
   const [username, setusername] = useState("");
+    const [name , setname ] = useState('')
   const [password, setpassword] = useState("");
   const [confirmpassword, setconfirmpassword] = useState("");
   const [isLoading, setisLoading] = useState(false);
   const [showPassword1, setshowpassword1] = useState(false);
   const [showPassword2, setshowpassword2] = useState(false);
+    const navigate =  useNavigate()
 
   const SubmitHandler = async () => {
-    let url = `${BASE_URL}/v8/user/signup?username=${username}?password=${password}`;
-    const signup_response = await fetch(url, {
+    let url = `${BASE_URL}/v8/user/sign_up?username=${username}&password=${password}&name=${name}`; const signup_response = await fetch(url, {
       method: "GET",
     });
     const { data, error } = await signup_response.json();
     if (signup_response.status !== 200) return console.error(error);
     console.log(data);
+        toast.success(data,toastifyoption)
+        
+         navigate('/v5/user/signup')
+        
   };
 
   return (
@@ -35,8 +42,20 @@ function Signup() {
               SubmitHandler();
             }}
           >
+<div className="user_name_field my-1">
+              <label className="block py-1 text-gray-700">Name</label>
+              <input
+                value={name}
+                onChange={(e) =>setname(e.target.value)}
+                type="text"
+                className="border border-gray-300 focus:border focus:border-blue-600 
+                px-4 py-1 outline-none rounded-md w-full"
+                placeholder="enter your username"
+              />
+            </div>
+
             <div className="user_name_field my-1">
-              <lable className="block py-1 text-gray-700">Email</lable>
+              <label className="block py-1 text-gray-700">Email</label>
               <input
                 value={username}
                 onChange={(e) => setusername(e.target.value)}
@@ -47,7 +66,7 @@ function Signup() {
               />
             </div>
             <div className="password_field my-1">
-              <lable className="block py-1 text-gray-700">password</lable>
+              <label className="block py-1 text-gray-700">password</label>
               <input
                 value={password}
                 onChange={(e) => setpassword(e.target.value)}
