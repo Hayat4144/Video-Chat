df6cca9f94d2e4f198c9a94c97562b7631a5e89f
Hayat ilyas <ihayat855@gmail.com>
2023-04-13 08:22:04 +0530

    adding chat image

--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -5,9 +5,13 @@ import VideoCom from "./VideoCom";
 const Home = () => {
   return (
     <Fragment>
-      <main className="home_page flex">
+      <main className="md:flex">
         <AsideNavbar className="aside" />
-        <div className="home_page">Start conversation with your friend</div>
+        <div className="home_page relative">
+          <h1 className="text-5xl font-bold text-blue-800 text-center mt-5">
+            Start convesation with your friend
+          </h1>
+        </div>
       </main>
     </Fragment>
   );
